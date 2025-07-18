472fb2fc2423b64ae15df7123ca475d107715ebe
Ivo Fernandes <ivomuze@gmail.com>
2025-07-18 23:32:03 +0100

    more customisable now

--- lib/src/ui/buttons/my_back_button.dart
+++ lib/src/ui/buttons/my_back_button.dart
@@ -3,24 +3,26 @@ import 'package:interactive_i18n/src/ui/buttons/round_button_ui.dart';
 
 class MyBackButton extends StatelessWidget {
   final GestureTapCallback? onBack;
+  final double? width;
+  final double? height;
 
   const MyBackButton({
     super.key,
     this.onBack,
+    this.width = 40,
+    this.height = 40,
   });
 
   @override
   Widget build(BuildContext context) => RoundButtonUI(
+        onTap: onBack ?? () => Navigator.of(context).pop(),
         child: Container(
           margin: const EdgeInsets.only(left: 9),
-          width: 40,
-          height: 40,
-          child: InkWell(
-            onTap: onBack ?? () => Navigator.of(context).pop(),
-            child: Icon(
-              Icons.arrow_back_ios,
-              color: Theme.of(context).colorScheme.primary,
-            ),
+          width: width,
+          height: height,
+          child: Icon(
+            Icons.arrow_back_ios,
+            color: Theme.of(context).colorScheme.primary,
           ),
         ),
       );
--- lib/src/ui/buttons/round_button_ui.dart
+++ lib/src/ui/buttons/round_button_ui.dart
@@ -1,43 +1,66 @@
 import 'package:flutter/material.dart';
 
+/// A customizable round button widget that can contain a child widget,
+/// has a specified width and height, and can handle tap events.
 class RoundButtonUI extends StatelessWidget {
+  /// child is the widget that will be displayed inside the round button.
   final Widget? child;
+
+  /// width is the width of the round button. If not provided, defaults to 40.
   final double? width;
+
+  /// height is the height of the round button. If not provided, defaults to 40.
   final double? height;
+
+  /// onTap is the callback function that will be executed when the button is tapped.
+  final GestureTapCallback? onTap;
+
+  /// border is an optional border for the round button. If not provided, a default border will be used.
+  final Border? border;
+
+  /// backgroundColor is the background color of the round button. If not provided, defaults to the surface color of the theme.
+  final Color? backgroundColor;
 
   const RoundButtonUI({
     this.child,
-    this.width,
-    this.height,
+    this.width = 40,
+    this.height = 40,
+    this.onTap,
+    this.border,
+    this.backgroundColor,
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) => Stack(
-        children: [
-          Material(
-            elevation: Theme.of(context).cardTheme.elevation ?? 5,
-            shape: const CircleBorder(),
-            child: Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Theme.of(context).colorScheme.surface,
-                border: Border.all(
-                  color: Theme.of(context).colorScheme.primary,
-                  width: 1,
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: onTap,
+        child: Stack(
+          children: [
+            Material(
+              elevation: Theme.of(context).cardTheme.elevation ?? 5,
+              shape: const CircleBorder(),
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: backgroundColor ?? Theme.of(context).colorScheme.surface,
+                  border: border ??
+                      Border.all(
+                        color: Theme.of(context).colorScheme.primary,
+                        width: 1,
+                      ),
                 ),
+                width: width,
+                height: height,
               ),
-              width: width ?? 40,
-              height: height ?? 40,
             ),
-          ),
-          ClipOval(
-            child: SizedBox(
-              width: width ?? 40,
-              height: height ?? 40,
-              child: child,
-            ),
-          )
-        ],
+            ClipOval(
+              child: SizedBox(
+                width: width,
+                height: height,
+                child: child,
+              ),
+            )
+          ],
+        ),
       );
 }
